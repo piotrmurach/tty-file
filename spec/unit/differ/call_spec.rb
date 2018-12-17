c677b075b36244afd116a6f3cdd01661577a043a
Piotr Murach <pmurach@gmail.com>
2018-12-17 21:58:40 +0100

    Change to freeze strings

--- spec/unit/differ/call_spec.rb
+++ spec/unit/differ/call_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe TTY::File::Differ, '#call' do
   it "diffs identical content" do
--- spec/unit/tail_file_spec.rb
+++ spec/unit/tail_file_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe TTY::File, '#tail_file' do
   it "tails file for lines with chunks smaller than file size" do
